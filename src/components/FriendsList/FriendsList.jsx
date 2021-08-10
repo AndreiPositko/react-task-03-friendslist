fc0e341030f65ddfeba0ba78bd2bf67bb1f3ff33
Andrey <andrei.positko.gb@gmail.com>
2021-08-10 17:07:10 +0300

    create FriendsList component

---
+++ src/components/FriendsList/FriendsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as Styled from './styled';
+
+const FriendsList = () =>  {
+    return (
+        <Styled.Container>
+            <Styled.Profile>
+                Hello
+            </Styled.Profile>
+        </Styled.Container>
+    )
+}
+
+export default FriendsList;
---
+++ src/components/FriendsList/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   max-width: 769px;
+   margin: 0 auto;
+   min-height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`;
+
+export const Profile = styled.div`
+   width: 350px;
+   min-height: 550px;
+   background-color: #fff;
+   box-shadow: 0 0 10px rgba(34, 60, 80, 0.2);
+   padding: 10px;
+`;
